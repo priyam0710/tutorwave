d2f3cfedaf7c6d434db488c301f9f66a4903f1bd
Priyanshu Singh <rajputpriyanshu1064@gmail.com>
2026-08-28 22:58:04 +0530

    Update tutors.ts

--- src/lib/data/tutors.ts
+++ src/lib/data/tutors.ts
@@ -246,7 +246,7 @@ export const tutors: Tutor[] = [
       'Dwarka Sector 2',
     ],
     teachingMode: ['home', 'online'],
-    verified: false,
+    unverified: true,
     rating: 5,
     reviewCount: 2,
     bio: '',
