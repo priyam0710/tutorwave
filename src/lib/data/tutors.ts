dc9e25be9bc00f624daf06be8a07e6c973496a1b
Priyanshu Singh <rajputpriyanshu1064@gmail.com>
2026-09-09 15:14:36 +0530

    Update tutor photo URL for Naveen Bhatnagar

--- src/lib/data/tutors.ts
+++ src/lib/data/tutors.ts
@@ -234,7 +234,7 @@ export const tutors: Tutor[] = [
     id: 'tutor-008',
     name: 'Naveen Bhatnagar',
     slug: 'naveen-bhatnagar',
-    photo: "https://img.rocket.new/generatedImages/rocket_gen_img_1e7575f93-1765298093270.png",
+    photo: "/assets/images/naveenbhatnagar.png",
     photoAlt: 'Naveen Bhatnagar',
     subjects: ['Maths','Science','Social Science','English','Accounts','Legal Studies','Economics','Business Studies',],
     classes: ['Class 6','Class 7','Class 8', 'Class 9', 'Class 10','Class 11','Class 12',],
