bd47d0772cafbc3e6f3fb0059bbfe1f1f8411095
Priyanshu Singh <rajputpriyanshu1064@gmail.com>
2026-09-15 12:32:34 +0530

    Update HomeFooter.tsx

--- src/components/HomeFooter.tsx
+++ src/components/HomeFooter.tsx
@@ -23,8 +23,8 @@ export default function HomeFooter() {
                 <Image
                   src="/assets/images/TutorWave_logo-1786512070481.webp"
                   alt="TutorWave logo"
-                  width={32}
-                  height={29}
+                  width={25}
+                  height={25}
                   className="object-contain"
                 />
               </div>
